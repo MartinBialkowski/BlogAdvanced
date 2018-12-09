2dd81757bbeae7bd0389da91314daca91e0a86cb
Martin Białkowski <embe2sc@gmail.com>
2018-12-09 22:59:00 +0100

    Added new entities

---
+++ BlogPost.Core/Entities/StudentCourse.cs
@@ -0,0 +1,15 @@
+﻿using BlogPost.Core.Entities;
+
+namespace BlogPost.Core
+{
+    public class StudentCourse
+    {
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public int AssessmentId { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+        public Assessment Assessment { get; set; }
+        public float Mark { get; set; }
+    }
+}
