0ef6d1d9c0af779710f667ee2ca985f46793f049
Martin Białkowski <embe2sc@gmail.com>
2019-06-23 14:31:56 +0200

    Added fluent validation + tests

---
+++ BlogPost.WebApi.Types.Student.Tests/UpdateStudentValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation.TestHelper;
+using Xunit;
+
+namespace BlogPost.WebApi.Types.Student.Tests
+{
+    public class UpdateStudentValidatorTests
+    {
+        private readonly UpdateStudentValidator validator;
+
+        public UpdateStudentValidatorTests()
+        {
+            validator = new UpdateStudentValidator();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("  ")]
+        [InlineData("")]
+        public void InvalidWhenNameIsNotValid(string name)
+        {
+            validator.ShouldHaveValidationErrorFor(x => x.Name, name);
+        }
+
+        [Fact]
+        public void ValidWhenNameProvided()
+        {
+            const string validName = "Name";
+            validator.ShouldNotHaveValidationErrorFor(x => x.Name, validName);
+        }
+    }
+}
---
+++ BlogPost.WebApi.Types/ValidatorFactory.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogPost.WebApi.Types
+{
+    public class ValidatorFactory : IValidatorFactory
+    {
+        private readonly IComponentContext container;
+
+        public ValidatorFactory(IComponentContext container)
+        {
+            this.container = container;
+        }
+
+        public IValidator<T> GetValidator<T>()
+        {
+            return (IValidator<T>)GetValidator(typeof(T));
+        }
+
+        public IValidator GetValidator(Type type)
+        {
+            var genericType = typeof(IValidator<>).MakeGenericType(type);
+            if (container.TryResolve(genericType, out object validator))
+            {
+                return (IValidator)validator;
+            }
+            return null;
+        }
+    }
+}
